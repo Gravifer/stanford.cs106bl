0b1564d4880bfac3ba89b811bd254c9080b387ce
Gravifer <44160838+Gravifer@users.noreply.github.com>
2025-10-14 16:56:58 +0800

    feat(asg4): redo part 1 - step 1,2

--- assignment4/spellcheck.cpp
+++ assignment4/spellcheck.cpp
@@ -14,12 +14,20 @@ Corpus tokenize(std::string& source) { // * see <https://en.cppreference.com/w/c
   auto spaces = find_all(source.begin(), source.end(), // ::isspace); 
     [](unsigned char c){ return std::isspace(c); }); // static_cast<int(*)(int)>(std::isspace));
   Corpus tokens;
-  auto last = source.begin();
-  for (auto it : spaces) {
-    tokens.emplace(last, it);
-    last = it + 1;
-  }
-  tokens.emplace(last, source.end());
+  // auto tokenStart = source.begin();
+  // for (auto space : spaces) {
+  //   if (tokenStart < space)  // deal with consecutive spaces
+  //       tokens.emplace(source, tokenStart, space);
+  //   tokenStart = space + 1;
+  // }
+  // tokens.emplace(source, tokenStart, source.end());
+  auto _ = std::transform(
+    spaces.begin(), spaces.end(), spaces.begin() + 1,
+    std::inserter(tokens, tokens.end()),
+    [&source](auto space, auto nextSpace) {
+      return Token{source, space + 1, nextSpace};
+    }
+  );
   return tokens;
 }
 
