28cf078c0fa547e0fc9fa760a021094f926d0bf5
Gravifer <44160838+Gravifer@users.noreply.github.com>
2025-10-15 01:13:14 +0800

    feat(asg5): in part 2 - rule of three

--- assignment5/user.cpp
+++ assignment5/user.cpp
@@ -64,6 +64,68 @@ void User::set_friend(size_t index, const std::string& name)
  * The definitions for your custom operators and special member functions will go here!
  */
 
+User::~User() {
+  delete[] _friends;
+}
+
+User::User(const User& other)
+  : _name(other._name)
+  , _friends(other._friends ? new std::string[other._capacity] : nullptr)
+  , _size(other._size)
+  , _capacity(other._capacity) 
+{
+  if (_friends) {
+    std::copy(other._friends, other._friends + _size, _friends);
+  }
+}
+
+User& User::operator=(const User& other) {
+  if (this != &other) { //* notice how much shorter copy-and-swap is!
+    // User(other).swap(*this); //* see <https://en.wikibooks.org/wiki/More_C++_Idioms/Copy-and-swap>
+    { // Create new resources BEFORE modifying state
+      std::string new_name = other._name; //! Rarely throws
+      std::string* new_friends = _friends; // Keep existing allocation if possible
+      size_t new_capacity = _capacity;
+      
+      if (_capacity < other._capacity) { //* then 0 < other._capacity
+        new_friends = new std::string[other._capacity]; //! May throw
+        new_capacity = other._capacity;
+      }
+      
+      // Copy friends data if needed
+      if (other._friends && new_friends) {
+        std::copy(other._friends, other._friends + other._size, 
+          new_friends); //! May throw
+      } else if (!other._friends) {
+        // Handle case where other has no friends - we should also have none
+        new_friends = nullptr;
+        new_capacity = 0;
+      }
+      
+      // Only modify state after all allocations succeed
+      _name = std::move(new_name);
+      if (new_friends != _friends) {
+        delete[] _friends;
+        _friends = new_friends;
+        _capacity = new_capacity;
+      }
+      _size = other._size;
+    }
+  }
+  return *this;
+}
+
+void User::swap(User& other) noexcept {
+  using std::swap;
+  swap(_name, other._name);
+  swap(_friends, other._friends);
+  swap(_size, other._size);
+  swap(_capacity, other._capacity);
+}
+inline void swap(User& first, User& second) noexcept {
+  first.swap(second);
+}
+
 /** Overloaded output stream operator for User
  * @example std::cout << user; // User(name=Alice, friends=[Bob, Charlie])
  */
--- assignment5/user.h
+++ assignment5/user.h
@@ -8,6 +8,16 @@
 
 class User
 {
+  /**
+   * Invariants:
+   * - `_name` is a valid `std::string`
+   * - `_friends` is either `nullptr` or to a valid array of `std::strings`
+   * - `_size` is the number of friends currently stored in `_friends`
+   * - `_capacity` is the size of the allocated array backing `_friends`
+   * - `_size <= _capacity`
+   * - If `_friends` is `nullptr`, then both `_size` and `_capacity` are 0
+   * - Whenever `_capacity` is 0, `_friends` is `nullptr`
+   */
 public:
   User(const std::string& name);
   void add_friend(const std::string& name);
@@ -19,7 +29,12 @@ public:
    * STUDENT TODO:
    * Your custom operators and special member functions will go here!
    */
+  ~User(); // destructor
+  User(const User& other); // copy constructor
+  User& operator=(const User& other); // copy assignment
   friend std::ostream& operator<<(std::ostream& os, const User& user);
+  void swap(User& other) noexcept; // swap function
+  friend inline void swap(User& first, User& second) noexcept;
 
 private:
   std::string _name;
